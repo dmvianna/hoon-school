cacc03b51579ee1bbaf9ef5085be809c4d2b33f3
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-09-26 15:35:17 +1000

    initial push/pop app

--- app-school/hw4/echo/ui/src/app.jsx
+++ app-school/hw4/echo/ui/src/app.jsx
@@ -1,44 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import Urbit from '@urbit/http-api';
-import { scryCharges } from '@urbit/api';
-import { AppTile } from './components/AppTile';
+import Urbit from '@urbit/http-api'
+import React, { useEffect, useReducer, useState } from 'react'
+import { AppTile } from './components/AppTile'
 
-const api = new Urbit('', '', window.desk);
-api.ship = window.ship;
+const api = new Urbit('', '', window.desk)
+api.ship = window.ship
+
+function reducer(state, action) {
+    let newState = [...state]
+    switch (action.type) {
+        case 'init':
+            return action.init
+        case 'push':
+            newState.unshift(action.val)
+            return newState
+        case 'pop':
+            newState.shift()
+            return newState
+        default:
+            return state
+    }
+}
 
 export function App() {
-  const [apps, setApps] = useState();
+    const [state, dispatch] = useReducer(reducer, [])
+    const [inputValue, setInputValue] = useState("")
 
-  useEffect(() => {
-    async function init() {
-      const charges = (await api.scry(scryCharges)).initial;
-      setApps(charges);
+    useEffect(() => {
+        async function init() {
+            api.subscribe({ app: "echo", path: '/values', event: handleUpdate })
+        }
+        init()
+    }, [])
+
+    const handleUpdate = (upd) => {
+        if ('init' in upd) {
+            dispatch({ type: 'init', init: upd.init })
+        }
+        else if ('push' in upd) {
+            dispatch({ type: 'push', val: upd.push.value })
+        }
+        else if ('pop' in upd) {
+            dispatch({ type: 'pop' })
+        }
     }
 
-    init();
-  }, []);
+    const push = () => {
+        const val = parseInt(inputValue)
+        if (isNaN(val)) return
+        api.poke({
+            app: 'echo',
+            mark: 'echo-action',
+            json: { push: { target: `~${window.ship}`, value: val } }
+        })
+        setInputValue("")
+    }
 
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <div className="max-w-md space-y-6 py-20">
-        <h1 className="text-3xl font-bold">Welcome to echo</h1>
-        <p>Here&apos;s your urbit&apos;s installed apps:</p>
-        {apps && (
-          <ul className="space-y-4">
-            {Object.entries(apps).map(([desk, app]) => (
-              <li key={desk} className="flex items-center space-x-3 text-sm leading-tight">
-                <AppTile {...app} />
-                <div className="flex-1 text-black">
-                  <p>
-                    <strong>{app.title || desk}</strong>
-                  </p>
-                  {app.info && <p>{app.info}</p>}
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </main>
-  );
+    const pop = () => {
+        api.poke({
+            app: 'echo',
+            mark: 'echo-action',
+            json: { pop: `~${window.ship}` }
+        })
+    }
+
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen">
+            <input style={{ width: 200 }} className='border' type='text' value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <div>
+                <button onClick={() => push()} style={{ width: 100 }} className='border p-2 text-black-400'>Push</button>
+                <button onClick={() => pop()} style={{ width: 100 }} className='border p-2 text-black-400'>Pop</button>
+                <p>Our stack</p>
+                {state.map((eachValue, index) => {
+                    return (<li key={index}>{eachValue}</li>)
+                })}
+            </div>
+        </main>
+    )
 }
+
+export default App;
